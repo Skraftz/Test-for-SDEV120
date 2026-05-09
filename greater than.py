8e89dcc9d23a4317f1c1120ba34853456e501d42
Skraftz <davekenkent@gmail.com>
2026-05-09 12:32:22 -0400

    Added more ways for testing.

--- greater than.py
+++ greater than.py
@@ -1,10 +1,12 @@
+import random
+
 def greaterThan(x, y):
     if x > y:
         return True
     return False
+while True:
+    a = random.randint(-100, 100)
+    b = random.randint(-150, 150)
+    c = greaterThan(a, b)
 
-a = 2
-b = 3
-c = greaterThan(2, 3)
-
-print(f"The statement {a} is greater than {b} is {c}")
+    print(f"The statement {a} is greater than {b} is {c}")
